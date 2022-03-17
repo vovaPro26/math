01437e5e53587b3251940b7bdf901d32b26a9fa2
Voffka <volodymyr.nik@gmail.com>
2022-03-17 11:43:18 +0200

    new information sklad from sklad to

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ const exersise = function () {
     }
     shareData = {
         title: "MDN",
-        text: `Правильних вдповідей ${trueCounter}.\n\rНеправильних відповідей ${falseCounter}.\n\rОцінка ${markResalt}.\n\rПриклади на ${plusTrue} ${minusTrue}`
+        text: `Правильних вдповідей ${trueCounter}.\n\rНеправильних відповідей ${falseCounter}.\n\rОцінка ${markResalt}.\n\rПриклади на ${plusTrue}, ${minusTrue}\n\r Діапазон чисел від ${skladFrom} до ${skladTo}`
       };
 
     let resultModel = {
